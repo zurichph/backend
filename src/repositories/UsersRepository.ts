9c18dbb983c83af5c1a11eec9bf8317d781059d3
wh1t3h47 <tom.mharres@gmail.com>
2020-11-06 22:56:17 -0300

    restore userRepository

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -2,22 +2,8 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Users from '../models/Users';
 import User from '../schemas/User';
-import options from '../config';
-
-const { jwtSecret } = options;
 
 class UsersRepository {
-  readonly hashTimeFiller: string;
-
-  readonly saltIterations: number = 13;
-
-  constructor() {
-    const timePaddingPassword = 'avoidUserEnumeration';
-    const salt = bcrypt.genSaltSync(this.saltIterations);
-    const fillerPassword = bcrypt.hashSync(timePaddingPassword, salt);
-    this.hashTimeFiller = fillerPassword;
-  }
-
   public async create({ username, password }: Users): Promise<Users> {
     const user = new Users({ username, password });
 
@@ -27,7 +13,7 @@ class UsersRepository {
       throw new Error('User already exists.');
     }
 
-    const salt = bcrypt.genSaltSync(this.saltIterations);
+    const salt = bcrypt.genSaltSync(13);
     const hashedPassword = bcrypt.hashSync(password, salt);
 
     try {
@@ -42,34 +28,14 @@ class UsersRepository {
     return user;
   }
 
-  private async consumeLoginTime(password: string): Promise<void> {
-    // a operacao terminaria muito antes se nao fizessemos um hashing,
-    // por exemplo, se o usuario fosse invalido, bcrypt nao consumiria
-    // esse tempo. essa parte evitaria entregar informacao que o usuario
-    // ta errado pelo tempo de resposta menor que se o usurio existisse
-    await bcrypt.compare(password, this.hashTimeFiller);
-  }
-
   public async login({ username, password }: Users): Promise<string> {
-    let passwordMatched = false;
     const user = await User.findOne({ username });
 
-    if (user) {
-      const storedPassword = user.toObject().password;
-      if (storedPassword) {
-        passwordMatched = await bcrypt.compare(password, storedPassword);
-      } else {
-        this.consumeLoginTime(password);
-      }
-    } else {
-      this.consumeLoginTime(password);
-    }
-
-    if (!passwordMatched || !user) {
+    if (!user || !bcrypt.compareSync(password, user.toObject().password || null)) {
       throw new Error('Verifique o usuário e senha.');
     }
 
-    const token = jwt.sign({ _id: user.toObject()._id }, jwtSecret, { expiresIn: '6h' });
+    const token = jwt.sign({ _id: user.toObject()._id }, 'dk@n1#id$un@2$jf@neij$fwj#kf@2k@f@ns', { expiresIn: '6h' });
 
     return token;
   }
